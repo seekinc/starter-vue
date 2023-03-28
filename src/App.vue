efb72fd7bf25e194bc9f1fdf40bd7a84cd82effa
seekinc <403359579@qq.com>
2023-03-29 05:49:36 +0800

    c

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  watch: {
-    '$route' () {
-      document.title = this.$route.meta.title || '地方特产分享网站' // 设置网页标题
-    }
+  name: 'App',
+  components: {
+    HelloWorld
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
